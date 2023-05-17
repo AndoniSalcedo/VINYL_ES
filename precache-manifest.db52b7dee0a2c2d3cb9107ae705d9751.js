a34b6ab4c49b80d75d6629259c9b9cc9c7705810
Andoni Salcedo Navarro <andonisalcedo@gmail.com>
2023-05-17 15:23:06 +0200

    Updates

---
+++ precache-manifest.db52b7dee0a2c2d3cb9107ae705d9751.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "01eb7bc19c0ddfe64fd2",
+    "url": "/AndoniSalcedo/VINYL_ES/static/css/main.7f0a3e23.chunk.css"
+  },
+  {
+    "revision": "01eb7bc19c0ddfe64fd2",
+    "url": "/AndoniSalcedo/VINYL_ES/static/js/main.01eb7bc1.chunk.js"
+  },
+  {
+    "revision": "ec78d838f430658199ce",
+    "url": "/AndoniSalcedo/VINYL_ES/static/js/1.ec78d838.chunk.js"
+  },
+  {
+    "revision": "441c30f5a402a2ea23cb",
+    "url": "/AndoniSalcedo/VINYL_ES/static/js/runtime~main.441c30f5.js"
+  },
+  {
+    "revision": "b9c27c1bf3534304d925e810cee0dc20",
+    "url": "/AndoniSalcedo/VINYL_ES/index.html"
+  }
+];
